b0e972cce052faee8f447760e65d0d1486fa17d5
akrivola <112705486+akrivola@users.noreply.github.com>
2024-05-16 00:05:38 +0300

    добавил цикл по страницам

--- Seminars/Seminar_2/HW_2.py
+++ Seminars/Seminar_2/HW_2.py
@@ -4,48 +4,59 @@ from fake_useragent import UserAgent
 from pprint import pprint
 import re
 
-url = "https://books.toscrape.com/"
+# https://books.toscrape.com/catalogue/page-1.html
+url = "https://books.toscrape.com/catalogue/"
+direct_url = "https://books.toscrape.com/catalogue/page-"
 
 headers = {
     "User-Agent": 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Mobile Safari/537.36'}
-# params = {"ref_": "bo_nb_hm_tab"}
 
 session = requests.session()
 
-# response = session.get(url, params=params, headers=headers)
-response = session.get(url, headers=headers)
-soup = BeautifulSoup(response.text, "html.parser")
-books = soup.find_all('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
-
 all_books = []
-for book in books:
-    book_info = {}
+page = 49
 
-    container = book.find('div', {'class': 'image_container'})
+while True:
+    response = session.get(direct_url + str(page) + '.html', headers=headers)
+    if response.status_code == 404:
+        break
+    soup = BeautifulSoup(response.text, "html.parser")
+    books = soup.find_all('li', {'class': 'col-xs-6 col-sm-4 col-md-3 col-lg-3'})
 
-    link = url + container.find('a').get('href')
+    for book in books:
+        book_info = {}
 
-    title = container.find('img').get('alt')
-    book_info['title'] = title
+        container = book.find('div', {'class': 'image_container'})
 
-    price = book.find('p', {'class': 'price_color'}).getText()[1:]
-    book_info['price'] = price
+        link = url + container.find('a').get('href')
 
-    response_inner = session.get(link, headers=headers)
-    soup_inner = BeautifulSoup(response_inner.text, "html.parser")
+        title = container.find('img').get('alt')
+        book_info['title'] = title
 
-    stock = soup_inner.find('p', {'class': 'instock availability'}).getText()
-    result = re.search(r'\d+', stock)
-    if result:
-        quantity = int(result.group())
-    else:
-        quantity = 0
+        price = book.find('p', {'class': 'price_color'}).getText()[1:]
+        book_info['price'] = price
 
-    book_info['quantity'] = quantity
+        response_inner = session.get(link, headers=headers)
+        soup_inner = BeautifulSoup(response_inner.text, "html.parser")
 
-    description = soup_inner.find('article', {'class': 'product_page'}).find('p',{'class': ''}).getText()
-    book_info['description'] = description
+        stock = soup_inner.find('p', {'class': 'instock availability'}).getText()
+        result = re.search(r'\d+', stock)
+        if result:
+            quantity = int(result.group())
+        else:
+            quantity = 0
 
-    all_books.append(book_info)
+        book_info['quantity'] = quantity
+        try:
+            description = soup_inner.find('article', {'class': 'product_page'}).find('p', {'class': ''}).getText()
+        except:
+            print(f'No description for book {title}')
+            description = ''
+
+        book_info['description'] = description
+
+        all_books.append(book_info)
+    print(f'Page {page} parced successfully')
+    page += 1
 
 pprint(all_books)
