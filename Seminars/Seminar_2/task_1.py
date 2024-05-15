0113f41b57e9443c78b8af818ca42658e96f6c33
A.Krivolapov <akrivola@ya.ru>
2024-05-15 16:57:48 +0300

    Семинар 2

--- Seminars/Seminar_2/task_1.py
+++ Seminars/Seminar_2/task_1.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 # ua = UserAgent()
+from pprint import pprint
 
 # url = "https://www.boxofficemojo.com/intl/?ref_=bo_nb_hm_tab"
 url = "https://www.boxofficemojo.com"
@@ -21,5 +22,25 @@ for row in rows[2:]:
     film = {}
     area_info = row.find('td',{'class': 'mojo-field-type-area_id'}).find('a')
     film['area'] = [area_info.getText(), url + area_info.get('href')]
-    print()
+
+    weekend_info = row.find('td', {'class': 'mojo-field-type-date_inteval'}).findChildren()[0]
+    film['weekend'] = [weekend_info.getText(), url + weekend_info.get('href')]
+
+    film['releases'] = int(row.find('td', {'class': 'mojo-field-type-positive_integer'})).getText()
+
+    frelease_info = row.find('td', {'class': 'mojo-field-type-release'}).findChildren()[0]
+    film['frelease'] = [frelease_info.getText(), url + frelease_info.get('href')]
+
+    try:
+        distributor_info = row.find('td', {'class': 'mojo-field-type-studio'}).findChildren()[0]
+        film['distributor'] = [distributor_info.getText(), url + distributor_info.get('href')]
+    except:
+        print('Exception with frelease, object = ', film['frelease'])
+        film['distributor'] = 'None'
+
+    film['gross'] = int(row.find('td', {'class': 'mojo-field-type-positive_integer'})).getText()
+
+    films.append(film)
+
+pprint(films)
 # test_link = soup.find("a", {'class':'a-link-normal'})
