78b2e60541f29735132faf7691cb82defbbffd3e
A.Krivolapov <akrivola@ya.ru>
2024-05-27 08:51:34 +0300

    Семинар 5

--- Seminars/Seminar_5/jobparser/spiders/hhru.py
+++ Seminars/Seminar_5/jobparser/spiders/hhru.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.http import HtmlResponse
 
 
 class HhruSpider(scrapy.Spider):
@@ -6,5 +7,6 @@ class HhruSpider(scrapy.Spider):
     allowed_domains = ["hh.ru"]
     start_urls = ["https://spb.hh.ru/search/vacancy?text=Руководитель+ит+отдела&from=suggest_post&salary=&resume=7d231f00ff0c7ddfbc0039ed1f32624b485472&ored_clusters=true&area=2&hhtmFrom=vacancy_search_list&hhtmFromLabel=vacancy_search_line"]
 
-    def parse(self, response):
-        pass
+    def parse(self, response:HtmlResponse):
+        # pass
+        print(response.url, response.status)
