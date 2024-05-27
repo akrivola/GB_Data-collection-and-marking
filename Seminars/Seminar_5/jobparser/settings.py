b86014cab83ba83fe57d8e15f27c0a389a7e5a96
A.Krivolapov <akrivola@ya.ru>
2024-05-27 08:43:50 +0300

    Семинар 5

--- Seminars/Seminar_5/jobparser/settings.py
+++ Seminars/Seminar_5/jobparser/settings.py
@@ -11,12 +11,14 @@ BOT_NAME = "jobparser"
 
 SPIDER_MODULES = ["jobparser.spiders"]
 NEWSPIDER_MODULE = "jobparser.spiders"
+LOG_ENABLED = True
+LOG_LEVEL = "DEBUG"
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = False
+ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 16
